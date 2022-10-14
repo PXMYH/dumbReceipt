365305f13b3986ade7e73c8048505d8ca9606e14
Michael Hu <pxmyhdev@gmail.com>
2022-10-13 23:34:04 -0500

    batch process

--- src/adapters/veryfi_adaptor.py
+++ src/adapters/veryfi_adaptor.py
@@ -8,17 +8,25 @@ def veryfi_adaptor():
     api_key = os.getenv('VERYFI_API_KEY')
 
     # categories = ['Grocery', 'Utilities', 'Travel']
-    file_path = './data/broken.jpg'
+    files = os.listdir('./data/receipts')
+    data_path = "./data/receipts/"
+    print(f"files = {files}")
+    # file_path = './data/broken.jpg'
 
     # This submits document for processing (takes 3-5 seconds to get response)
     veryfi_client = Client(client_id, client_secret, username, api_key)
-    response = veryfi_client.process_document(file_path)
-    print("response from veryfi: ", response)
+    receipt_items = []
+    for file in files:
+        response = veryfi_client.process_document(data_path + file)
+        print("response from veryfi: ", response)
 
-    items = []
-    line_items = response['line_items']
-    for item in line_items:
-        print(f"item = {item}")
-        item_description = item['description']
-        items.append(item_description)
-    return items
+        items = []
+        line_items = response['line_items']
+        for item in line_items:
+            print(f"item = {item}")
+            item_description = item['description']
+            items.append(item_description)
+
+        receipt_items.append(items)
+
+    return receipt_items
