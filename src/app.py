3c578fd86c9790d0aa426ac7583b13f516679b67
Michael Hu <pxmyhdev@gmail.com>
2022-10-30 22:43:26 -0500

    db connection works

--- src/app.py
+++ src/app.py
@@ -6,13 +6,12 @@ from werkzeug.utils import secure_filename
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import text
 
-# db = SQLAlchemy()
 app = Flask(__name__)
 
-# db_name = 'receipt.db'
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + db_name
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-# db.init_app(app)
+db_name = 'receipt.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + db_name
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db = SQLAlchemy(app)
 
 @app.route('/upload')
 def upload_file():
@@ -31,14 +30,14 @@ def main():
     receiptEngine = Receipt.Receipt()
 
     # connect to database
-    # try:
-    #     db.session.query(text('1')).from_statement(text('SELECT 1')).all()
-    #     return '<h1>It works.</h1>'
-    # except Exception as e:
-    #     # e holds description of the error
-    #     error_text = "<p>The error:<br>" + str(e) + "</p>"
-    #     hed = '<h1>Something is broken.</h1>'
-    #     return hed + error_text
+    try:
+        db.session.query(text('1')).from_statement(text('SELECT 1')).all()
+        return '<h1>It works.</h1>'
+    except Exception as e:
+        # e holds description of the error
+        error_text = "<p>The error:<br>" + str(e) + "</p>"
+        hed = '<h1>Something is broken.</h1>'
+        return hed + error_text
 
     try:
         # start the receipt import engine
