0a9ae61d1eecfda7fd6f8f38a26592aee25151e2
Michael Hu <pxmyhdev@gmail.com>
2022-11-08 00:16:11 -0600

    save to database

--- src/business/recognize.py
+++ src/business/recognize.py
@@ -1,17 +1,21 @@
-from adapters.midee_adapter import mindee_adaptor
-from adapters.veryfi_adaptor import veryfi_adaptor
-
+from adapters import mindee_adapter, veryfi_adapter
 import configs.app_config as app_config
+from db.models.items import Items
+from db.db_instance import db
 
 
 def recognize():
     print("recognizing ...")
 
-    mode = "VERYFI"
-
-    if app_config.OCR_ENGINE_SUPPLIER == mode:
-        items = veryfi_adaptor()
+    if app_config.OCR_ENGINE_SUPPLIER == "VERYFI":  # TODO: move "VERYFI" as enum
+        items = veryfi_adapter()
         print(f"recognized items = {items}")
+
+        # save to database
+        item = Items(name="Test", price=1.2, quantity=1, vendor="Test Vendor")
+        db.session.add(item)
+        db.session.commit()
+
         return items
     else:
-        mindee_adaptor()
+        mindee_adapter()
