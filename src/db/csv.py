fead372acab96304c92c74331ffbc0fd129429b3
Michael Hu <pxmyhdev@gmail.com>
2022-11-25 22:38:17 -0600

    write to csv

---
+++ src/db/csv.py
@@ -0,0 +1,8 @@
+import csv
+
+
+def write_csv(items):
+    with open("receipt_records.csv", "a") as csvfile:
+        receipt_writer = csv.writer(csvfile, delimiter=" ", quotechar="|")
+        for item in items:
+            receipt_writer.writerow([item])
