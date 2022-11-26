b37e296741a35016fc77d7dd8222a50296d241ac
Michael Hu <pxmyhdev@gmail.com>
2022-11-25 22:38:32 -0600

    write to csv

--- src/business/recognize.py
+++ src/business/recognize.py
@@ -1,5 +1,6 @@
 from adapters.midee_adapter import mindee_adapter
 from adapters.veryfi_adapter import veryfi_adapter
+from db.csv import write_csv
 
 import configs.app_config as app_config
 
@@ -10,6 +11,9 @@ def recognize():
     if app_config.OCR_ENGINE_SUPPLIER == "VERYFI":  # TODO: move "VERYFI" as enum
         items = veryfi_adapter()
         print(f"recognized items = {items}")
+
+        # write to csv file
+        write_csv(items)
         return items
     else:
         mindee_adapter()
