e19cfb5d1b4a874bc10b58d9cc38254435ed5d5a
Michael Hu <pxmyhdev@gmail.com>
2022-12-10 23:34:03 -0600

    Write to csv

--- src/db/csv.py
+++ src/db/csv.py
@@ -3,6 +3,6 @@ import csv
 
 def write_csv(items):
     with open("receipt_records.csv", "a") as csvfile:
-        receipt_writer = csv.writer(csvfile, delimiter=" ", quotechar="|")
+        receipt_writer = csv.writer(csvfile, delimiter=" ")
         for item in items:
-            receipt_writer.writerow([item])
+            receipt_writer.writerow(item)
