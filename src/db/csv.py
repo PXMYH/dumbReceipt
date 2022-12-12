d45807fcd993a561cd9498ef3f491450e4c8496f
Michael Hu <pxmyhdev@gmail.com>
2022-12-11 23:50:24 -0600

    Add processing for multiple items on the same receipt

--- src/db/csv.py
+++ src/db/csv.py
@@ -5,4 +5,5 @@ def write_csv(items):
     with open("receipt_records.csv", "a") as csvfile:
         receipt_writer = csv.writer(csvfile, delimiter=" ")
         for item in items:
+            print(f"preparing to write item {item}")
             receipt_writer.writerow(item)
