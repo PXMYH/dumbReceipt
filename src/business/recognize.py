82b8dcd27839cf0b13c49edf3afa0afd7a489781
Michael Hu <pxmyhdev@gmail.com>
2022-12-27 03:19:45 -0600

    Writing items to database

--- src/business/recognize.py
+++ src/business/recognize.py
@@ -1,6 +1,9 @@
 from adapters.midee_adapter import mindee_adapter
 from adapters.veryfi_adapter import veryfi_adapter
 from db.csv import write_csv
+from db.models import Product, db
+from datetime import datetime
+
 import csv
 
 import configs.app_config as app_config
@@ -19,6 +22,12 @@ def remove_duplicates(items):
     return items
 
 
+def convert_datetime(datetime_string):
+    datetime_format = "%Y-%m-%d %H:%M:%S"
+    converted_datetime = datetime.strptime(datetime_string, datetime_format)
+    return converted_datetime
+
+
 def recognize():
     print("recognizing ...")
 
@@ -31,6 +40,20 @@ def recognize():
 
         # write to csv file
         write_csv(items)
+
+        # write to database
+        for item in items:
+            product = Product(
+                name=item[-2],
+                quantity=item[-1],
+                price=item[3],
+                vendor=item[7],
+                created_at=convert_datetime(item[0]),
+            )
+            # Add the product to the database, not efficient, should explore batch write
+            db.session.add(product)
+            db.session.commit()
+
         return items
     else:
         mindee_adapter()
--- src/db/models.py
+++ src/db/models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 db = SQLAlchemy()
 print("connecting to db")
@@ -7,11 +8,11 @@ print("connecting to db")
 class Product(db.Model):
     __tablename__ = "products"
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80))
-    quantity = db.Column(db.Integer)
-    price = db.Column(db.Float)
-    vendor = db.Column(db.String(80))
-    created_at = db.Column(db.DateTime)
+    name = db.Column(db.String, nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    vendor = db.Column(db.String, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
     def __repr__(self):
         return (
