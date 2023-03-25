06cdb7be046b78c4ff51dc9dc20edf6aa68b2f8c
Michael Hu <pxmyhdev@gmail.com>
2023-03-25 16:59:33 -0500

    refactor

--- db/connect.py
+++ db/connect.py
@@ -2,15 +2,14 @@ from .models import db, Product
 
 
 def connect_database(app):
-    db_name = "receipt.db"
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///receipt.db"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + db_name
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
-    db.init_app(app)
-
-    try:
-        with app.app_context():
+    with app.app_context():
+        try:
+            db.init_app(app)
+            db.create_all()
             products = Product.query.all()
             print(f"Products: {products}")
-    except Exception as e:
-        raise ConnectionError(f"Connection to database failed, {e}")
+        except Exception as e:
+            raise ConnectionError(f"Connection to database failed, {e}")
