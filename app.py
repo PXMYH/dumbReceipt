81fc48350bee2978c78d6a8d4f6990cda14b43e6
Michael Hu <pxmyhdev@gmail.com>
2023-03-25 16:32:25 -0500

    Revert "refactor app.py"
    This reverts commit 85569b5df5a01a3e93a17f067ddf7fb0c8beb28f.

--- app.py
+++ app.py
@@ -1,41 +1,29 @@
-import logging
 import os
-from pathlib import Path
-from typing import List, Tuple
 from flask import Flask, flash, render_template, request, redirect, url_for
 from werkzeug.utils import secure_filename
 from db.connect import connect_database
 from controllers import Receipt
 import configs.app_config as app_config
 
-UPLOAD_FOLDER = os.environ.get("UPLOAD_FOLDER", "./uploads/receipts")
+UPLOAD_FOLDER = "./uploads/receipts"
 ALLOWED_EXTENSIONS = {"txt", "pdf", "png", "jpg", "jpeg", "gif"}
 
 app = Flask(__name__)
-app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
 connect_database(app)
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
 
-def allowed_file(filename: str) -> bool:
+def allowed_file(filename):
     return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
 
-def remove_previous_receipt_files(receipt_dir: str) -> None:
-    """Remove existing receipt files except for .gitkeep"""
-    for f in os.listdir(receipt_dir):
-        if not f.endswith(".gitkeep"):
-            file_path = Path(receipt_dir) / f
-            with file_path.open("w") as f:
-                f.unlink()
 
 @app.route("/")
 def upload_page():
     return render_template("upload.html")
 
+
 @app.route("/", methods=["GET", "POST"])
-def uploader() -> str:
+def uploader():
     if request.method == "POST":
         # check if the post request has the file part
         if "file" not in request.files:
@@ -50,38 +38,44 @@ def uploader() -> str:
             return redirect(request.url)
 
         # remove existing receipts
-        receipt_dir = Path(app_config.RECEIPT_FILE_DIR).resolve()
-        logger.debug(f"removing previous receipt files in {receipt_dir}")
-        remove_previous_receipt_files(receipt_dir)
+        current_dir = os.path.dirname(__file__)
+        print(f"current directory: {current_dir}")
+        receipt_dir = current_dir + "/" + app_config.RECEIPT_FILE_DIR
+        print("removing previous receipt files...")
+        [
+            os.remove(receipt_dir + "/" + f)
+            for f in os.listdir(receipt_dir)
+            if not f.endswith(".gitkeep")
+        ]
 
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
-            file_path = receipt_dir / filename
-            file.save(file_path)
+            file.save(os.path.join(receipt_dir, filename))
             # TODO: replace with saving the file to a remote storage
             # return redirect(url_for("upload_page", name=filename))
             return redirect(url_for("process", name=filename))
 
         return f"file {secure_filename(file.filename)} uploaded successfully"
 
+
 @app.route("/process")
-def process() -> str:
+def process():
     # initialize receipt engine
-    receipt_engine = Receipt.Receipt()
+    receiptEngine = Receipt.Receipt()
 
     try:
         # start the receipt import engine
-        ingestion_result = receipt_engine.ingest()
+        ingestion_result = receiptEngine.ingest()
 
         # start the receipt processing engine
-        receipt_engine.run()
+        receiptEngine.run()
 
         return f"<div>Success! {ingestion_result} </div>"
 
     finally:
         # close the app if necessary
-        receipt_engine.shutdown()
+        receiptEngine.shutdown()
+
 
 if __name__ == "__main__":
-    logging.basicConfig(format="%(asctime)s - %(levelname)s - %(message)s")
     app.run(debug=True)
